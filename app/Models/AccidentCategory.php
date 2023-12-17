4d86fc7d62aff8cd10de0fb3c9c3b95df2ff0d9f
Nikolay <NikolayUlyanov@yandex.ru>
2023-12-17 21:43:08 +0300

    Create empty models, migrations, seeders

---
+++ app/Models/AccidentCategory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AccidentCategory extends Model
+{
+    use HasFactory;
+}
---
+++ database/seeders/SeveritySeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SeveritySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
